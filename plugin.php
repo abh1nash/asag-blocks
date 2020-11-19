95f0604df4746b6bbad8c3ca4bb876cd5d90696b
Abhinash Khatiwada <me@abhinash.net>
2020-11-19 20:46:08 +0545

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: ASAG Blocks
+ * Plugin URI: http://abhinash.net/
+ * Description: Custom Gutenberg Blocks for ASAG
+ * Author: Abhinash Khatiwada
+ * Author URI: https://abhinash.net/
+ * License: GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package ASAG
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
