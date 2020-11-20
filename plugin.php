b2666007879de4e901aa215e827f2d893997dab1
Abhinash Khatiwada <me@abhinash.net>
2020-11-20 10:47:40 +0545

    fix alert box and border colors

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ASAG Blocks
  * Plugin URI: http://abhinash.net/
- * Version: 1.0.0.1
+ * Version: 1.0.0.2
  * Description: Custom Gutenberg Blocks for ASAG
  * Author: Abhinash Khatiwada
  * Author URI: https://abhinash.net/
